6708bb676bf18962afa15611fc566d8b8a6bbc0c
oswaldodearmas <oswaldodearmas@gmail.com>
2024-09-02 14:16:52 +0200

    MainView Basics ongoing

--- Superheroes_IOS/Controllers/ListViewController.swift
+++ Superheroes_IOS/Controllers/ListViewController.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
+
+    @IBOutlet weak var heroesSearchBar: UISearchBar!
+    @IBOutlet weak var heroesTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
